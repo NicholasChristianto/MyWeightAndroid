ec1cb39d2f9e9891786e84cfc7ab3d87edacbfbe
nadiacarrisa <stephanie.nadia@ti.ukdw.ac.id>
2019-11-09 16:15:51 +0700

    logout

--- app/src/main/java/com/example/myweight/SettingFragment.java
+++ app/src/main/java/com/example/myweight/SettingFragment.java
@@ -1,19 +1,36 @@
 package com.example.myweight;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingFragment extends Fragment {
+   private Button btnlogout;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_settings,container,false);
+        btnlogout = v.findViewById(R.id.btnLogout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+                Toast.makeText(getActivity(), "User Sign out!", Toast.LENGTH_SHORT).show();
+                Intent intent1 = new Intent(v.getContext(), Login.class);
+                startActivity(intent1);
+            }
+        });
         return v;
     }
 }
