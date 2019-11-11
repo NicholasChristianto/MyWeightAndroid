170d1b0f1722ecf3d3d36822e2fbf466eb76e15e
nadiacarrisa <stephanie.nadia@ti.ukdw.ac.id>
2019-11-11 17:39:59 +0700

    set text email

--- app/src/main/java/com/example/myweight/SettingFragment.java
+++ app/src/main/java/com/example/myweight/SettingFragment.java
@@ -7,19 +7,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingFragment extends Fragment {
    private Button btnlogout;
+   private EditText stgnama;
+   private EditText stgemail;
+   private EditText stgberat;
+   private EditText stgtinggi;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_settings,container,false);
+        stgnama = v.findViewById(R.id.stgnama);
+        stgemail = v.findViewById(R.id.stgemail);
+        stgberat = v.findViewById(R.id.stgberat);
+        stgtinggi = v.findViewById(R.id.stgtinggi);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            stgnama.setText(name);
+            stgemail.setText(email);
+        }
         btnlogout = v.findViewById(R.id.btnLogout);
         btnlogout.setOnClickListener(new View.OnClickListener() {
             @Override
