0c030d3557f5ce610f99fec3e88c92af16cd8b09
Nicholas <nicholas.christianto@ti.ukdw.ac.id>
2019-11-16 16:00:16 +0700

    added chart(statis)

--- app/src/main/java/com/example/myweight/HomeFragment.java
+++ app/src/main/java/com/example/myweight/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.myweight;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -15,6 +16,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.content.Context.SENSOR_SERVICE;
 import static androidx.core.content.ContextCompat.getSystemService;
 
@@ -25,7 +36,7 @@ public class HomeFragment extends Fragment implements SensorEventListener, StepL
     private Sensor accel;
     private static final String TEXT_NUM_STEPS = "Number of Steps: ";
     private int numSteps;
-
+    private TextView labelWelcome;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,8 +47,53 @@ public class HomeFragment extends Fragment implements SensorEventListener, StepL
         simpleStepDetector.registerListener(this);
         TvSteps = (TextView) v.findViewById(R.id.step);
         sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+        LineChart lineChart = v.findViewById(R.id.lineChart);
+        List<Entry> lineEntries = getDataSet();
+        LineDataSet lineDataSet = new LineDataSet(lineEntries, "Berat");
+        lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        lineDataSet.setHighlightEnabled(true);
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setColor(Color.BLUE);
+        lineDataSet.setCircleColor(Color.GREEN);
+        lineDataSet.setCircleRadius(6);
+        lineDataSet.setCircleHoleRadius(3);
+        lineDataSet.setDrawHighlightIndicators(true);
+        lineDataSet.setHighLightColor(Color.RED);
+        lineDataSet.setValueTextSize(12);
+        lineDataSet.setValueTextColor(Color.DKGRAY);
+
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.getDescription().setText("Tanggal");
+        lineChart.getDescription().setTextSize(12);
+        lineChart.setDrawMarkers(true);
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+        lineChart.animateY(1000);
+        lineChart.getXAxis().setGranularityEnabled(true);
+        lineChart.getXAxis().setGranularity(1.0f);
+        lineChart.getXAxis().setLabelCount(lineDataSet.getEntryCount());
+        lineChart.setData(lineData);
         return v;
     }
+    private void drawLineChart() {
+
+    }
+    private List<Entry> getDataSet() {
+        List<Entry> lineEntries = new ArrayList<Entry>();
+        lineEntries.add(new Entry(0, 1));
+        lineEntries.add(new Entry(1, 2));
+        lineEntries.add(new Entry(2, 3));
+        lineEntries.add(new Entry(3, 4));
+        lineEntries.add(new Entry(4, 2));
+        lineEntries.add(new Entry(5, 3));
+        lineEntries.add(new Entry(6, 1));
+        lineEntries.add(new Entry(7, 5));
+        lineEntries.add(new Entry(8, 7));
+        lineEntries.add(new Entry(9, 6));
+        lineEntries.add(new Entry(10, 4));
+        lineEntries.add(new Entry(11, 5));
+        return lineEntries;
+    }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
